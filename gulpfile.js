982a8199c17dc75c9ae2c7a55ed1a5c2534509f1
Roy Calderon <royedc4@gmail.com>
2017-03-07 16:13:36 -0400

    Elements created. Form to control all the UI. Request to talk correctly with amazon server. Some styles to separate the element for easier scalability.
    Some development features added:
    Gulp, to watch and serve the elements and cross-browser sync.
    Other tasks could be created to vulcanize the element for example.

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var historyApiFallback = require('connect-history-api-fallback');
+
+var config = {
+  paths: {
+	html: ['./*.html'],
+	get allFiles(){
+	  return [].concat(this.html);
+	}
+  }
+};
+
+var server = {
+  dev: {
+	port: 5000,
+	logPrefix: 'Element',
+	server: {
+	  index: 'app.html',
+	  baseDir: '.'
+	},
+	files: config.paths.allFiles,
+	notify: false
+  },
+  get config(){
+	return this.dev;
+  }
+};
+
+gulp.task('browserSync', function(){
+  browserSync.init(server.config);
+});
+
+gulp.task('serve', gulp.series('browserSync'));
